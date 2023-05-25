22cd6d223ab0fa9afbbb11608d4a12d518d25546
victorandruscagit <victor.v.andrusca@gmail.com>
2023-05-25 17:16:41 +0300

    added product-list-table.component.html insetead of previous one

--- src/app/common/product.ts
+++ src/app/common/product.ts
@@ -5,6 +5,7 @@ export class Product {
         public description: string,
         public unitPrice: number,
         public imageUrl: string,
+        public active : boolean,
         public unitsInStock: number,
         public dateCreated: Date,
         public lastUpdated: Date
