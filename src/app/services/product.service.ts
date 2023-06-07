5e71a77baa77e7eb84881d9d06bc759b1f0694a3
victorandruscagit <victor.v.andrusca@gmail.com>
2023-06-07 15:52:15 +0300

    fixed duplicate method in product service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -46,14 +46,7 @@ export class ProductService {
     return this.getProducts(searchUrl);
   }
 
-  getProductListPaginate(thePage:number,
-                         thePageSize:number,
-                         theCategoryId: number
-                         ): Observable<GetResponseProducts> {
-    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}` + '&page=${thePage}&size=${thePageSize}';
-    return this.httpClient.get<GetResponseProducts>(searchUrl);
-    
-  }
+
 
   searchProducts(theKeyword: string): Observable<Product[]> {
 
