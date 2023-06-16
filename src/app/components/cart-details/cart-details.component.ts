f52951812b35b7fe2ead5c8d6d5a46d091721b89
victorandruscagit <victor.v.andrusca@gmail.com>
2023-06-16 13:00:28 +0300

    added table to  item details in cart-detail.component.html

--- src/app/components/cart-details/cart-details.component.ts
+++ src/app/components/cart-details/cart-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CartItem } from 'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-details',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-details.component.css']
 })
 export class CartDetailsComponent implements OnInit {
+  cartItems : CartItem[] = [];
+  totalPrice : number=0;
+  totalQuantity : number=0;
 
-  constructor() { }
+  constructor(private cartService : CartService) { }
 
   ngOnInit(): void {
+    this.listCartDetails();
+  }
+
+  
+  listCartDetails() {
+    this.cartItems = this.cartService.cartItems;
+    this.cartService.totalPrice.subscribe(
+      data =>this.totalPrice = data);
+
+      this.cartService.totalQuantity.subscribe(
+        data =>this.totalQuantity = data);
+
+        this.cartService.computeCartTotals();
+
+    
   }
 
 }
