4fac24ffe44856b4866fce4d4ef8a9a20fc4909b
victorandruscagit <victor.v.andrusca@gmail.com>
2023-06-29 09:28:19 +0300

    finished with billing address validation

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -46,8 +46,8 @@ export class CheckoutComponent implements OnInit {
         ),
 
         shippingAddress: this.formBuilder.group({
-          street: new FormControl('', [Validators.required, Validators.minLength(2),
-          ShopValidators.notOnlyWhiteSpaces]),
+          street: new FormControl('', [Validators.required, Validators.minLength(2),Validators.minLength(2), ShopValidators.notOnlyWhiteSpaces]),
+         
           city: new FormControl('', [Validators.required, Validators.minLength(2),
           ShopValidators.notOnlyWhiteSpaces]),
           state: new FormControl('', [Validators.required]),
@@ -57,13 +57,14 @@ export class CheckoutComponent implements OnInit {
         }),
         billingAddress: this.formBuilder.group(
           {
-            street: [''],
-            city: [''],
-            state: [''],
-            country: [''],
-            zipCode: ['']
-          }
-        ),
+            street: new FormControl('', [Validators.required, Validators.minLength(2),Validators.minLength(2), ShopValidators.notOnlyWhiteSpaces]),
+            city : new FormControl('', [Validators.required, Validators.minLength(2),
+              ShopValidators.notOnlyWhiteSpaces]),
+              state: new FormControl('', [Validators.required]),
+              country: new FormControl('', [Validators.required]),
+              zipCode: new FormControl('', [Validators.required, Validators.minLength(2),
+              ShopValidators.notOnlyWhiteSpaces])
+            }),
         creditCard: this.formBuilder.group(
           {
             cardType: [''],
@@ -110,11 +111,18 @@ export class CheckoutComponent implements OnInit {
   get firstName() { return this.checkoutFormGroup.get('customer.firstName'); }
   get lastName() { return this.checkoutFormGroup.get('customer.lastName'); }
   get email() { return this.checkoutFormGroup.get('customer.email'); }
+
   get shippingAddressStreet() { return this.checkoutFormGroup.get('shippingAddress.street'); }
   get shippingAddressCity() { return this.checkoutFormGroup.get('shippingAddress.city'); }
   get shippingAddressState() { return this.checkoutFormGroup.get('shippingAddress.state'); }
   get shippingAddressZipCode() { return this.checkoutFormGroup.get('shippingAddress.zipCode'); }
   get shippingAddressCountry() { return this.checkoutFormGroup.get('shippingAddress.country'); }
+
+  get billingAddressStreet() { return this.checkoutFormGroup.get('billingAddress.street'); }
+  get billingAddressCity() { return this.checkoutFormGroup.get('billingAddress.city'); }
+  get billingAddressState() { return this.checkoutFormGroup.get('billingAddress.state'); }
+  get billingAddressZipCode() { return this.checkoutFormGroup.get('billingAddress.zipCode'); }
+  get billingAddressCountry() { return this.checkoutFormGroup.get('billingAddress.country'); }
 
 
 
